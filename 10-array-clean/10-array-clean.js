41e6a45278e7caf2ccaae4898819ee5d9e5c9b2e
Your Name <you@example.com>
2026-06-13 13:28:30 +0300

    fix

--- 10-array-clean/10-array-clean.js
+++ 10-array-clean/10-array-clean.js
@@ -1,15 +1,15 @@
 const arr  = [3, 6, 9, 2];
 
 const filterArr = (isDeleted, arr) => {
+    let res = [];
     for (let i = 0; i < arr.length; i++) {
-        if (isDeleted(arr[i])) {
-            arr.splice(i, 1);
-            i--;
-           
+        if (!isDeleted(arr[i])) {
+            res.push(arr[i]);
         }
     }
+    return res;
 }
 
 const morethenFive = (num) => num > 5;
 
-filterArr(morethenFive, arr);
+console.log(filterArr(morethenFive, arr));
